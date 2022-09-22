eb536ee147e16b03ce6ef7013f4c892dd5ed4596
wheremyfoodat <gponiris2004@gmail.com>
2022-09-22 03:46:45 +0300

    Fiix std::filesystem::path being bad

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@ int main (int argc, char *argv[]) {
 
     auto elfPath = std::filesystem::current_path() / (argc > 1 ? argv[1] : "SimplerTri.elf");
     if (!emu.loadELF(elfPath)) {
-        Helpers::panic("Failed to load ELF file: %s", elfPath.c_str());
+        // For some reason just .c_str() doesn't show the proper path
+        Helpers::panic("Failed to load ELF file: %s", elfPath.string().c_str());
     }
 
     emu.run();
