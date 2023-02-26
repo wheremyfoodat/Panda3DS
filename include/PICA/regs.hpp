bf34410e4b3c2d0c113685e5c00eb115746f5e57
wheremyfoodat <gponiris2004@gmail.com>
2023-02-27 01:59:28 +0200

    [PICA] Blending register definitions

--- include/PICA/regs.hpp
+++ include/PICA/regs.hpp
@@ -13,6 +13,9 @@ namespace PICAInternalRegs {
 		ShaderOutputCount = 0x4F,
 
 		// Framebuffer registers
+		ColourOperation = 0x100,
+		BlendFunc = 0x101,
+		BlendColour = 0x103,
 		AlphaTestConfig = 0x104,
 		DepthAndColorMask = 0x107,
 		ColourBufferFormat = 0x117,
