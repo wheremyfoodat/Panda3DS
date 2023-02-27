d0eb284598e871d2b2c81c5f706b4bddebcc249b
wheremyfoodat <gponiris2004@gmail.com>
2023-02-27 19:50:18 +0200

    [Colour.hpp] Support for 6 to 8-bit colour

--- include/colour.hpp
+++ include/colour.hpp
@@ -3,11 +3,15 @@
 
 // Helpers functions for converting colour channels between bit depths
 namespace Colour {
-    inline static u8 convert4To8Bit(u8 c) {
+    inline static constexpr u8 convert4To8Bit(u8 c) {
         return (c << 4) | c;
     }
 
-    inline static u8 convert5To8Bit(u8 c) {
+    inline static constexpr u8 convert5To8Bit(u8 c) {
         return (c << 3) | (c >> 2);
+    }
+
+    inline static constexpr u8 convert6To8Bit(u8 c) {
+        return (c << 2) | (c >> 4);
     }
 }
