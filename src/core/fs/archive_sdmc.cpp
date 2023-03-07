b9de43d0c4668052c7b22e47e39641758ec5d502
wheremyfoodat <gponiris2004@gmail.com>
2023-03-07 22:26:45 +0200

    [SDMC] Add forgotten newlines

--- src/core/fs/archive_sdmc.cpp
+++ src/core/fs/archive_sdmc.cpp
@@ -17,11 +17,11 @@ FileDescriptor SDMCArchive::openFile(const FSPath& path, const FilePerms& perms)
 }
 
 ArchiveBase* SDMCArchive::openArchive(const FSPath& path) {
-	printf("SDMCArchive::OpenArchive: Failed");
+	printf("SDMCArchive::OpenArchive: Failed\n");
 	return nullptr;
 }
 
 std::optional<u32> SDMCArchive::readFile(FileSession* file, u64 offset, u32 size, u32 dataPointer) {
-	printf("SDMCArchive::ReadFile: Failed");
+	printf("SDMCArchive::ReadFile: Failed\n");
 	return std::nullopt;
 }
