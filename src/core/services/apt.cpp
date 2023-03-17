7834f6d734735bb148bca0a3e86b390092143a4c
wheremyfoodat <gponiris2004@gmail.com>
2023-03-17 02:46:11 +0200

    Stub APT::GetLockHandle to fail

--- src/core/services/apt.cpp
+++ src/core/services/apt.cpp
@@ -123,7 +123,7 @@ void APTService::getLockHandle(u32 messagePointer) {
 		lockHandle = kernel.makeMutex();
 	}
 
-	mem.write32(messagePointer + 4, Result::Success); // Result code
+	mem.write32(messagePointer + 4, Result::Failure); // Result code
 	mem.write32(messagePointer + 8, 0); // AppletAttr
 	mem.write32(messagePointer + 12, 0); // APT State (bit0 = Power Button State, bit1 = Order To Close State)
 	mem.write32(messagePointer + 16, 0); // Translation descriptor
