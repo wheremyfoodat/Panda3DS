82b986bbc0ef8339cddc97f4c9f85a2f3bb90fdf
wheremyfoodat <gponiris2004@gmail.com>
2023-04-10 21:55:14 +0300

    [Y2R] Add transfer event

--- src/core/services/service_manager.cpp
+++ src/core/services/service_manager.cpp
@@ -5,7 +5,7 @@
 ServiceManager::ServiceManager(std::array<u32, 16>& regs, Memory& mem, GPU& gpu, u32& currentPID, Kernel& kernel)
 	: regs(regs), mem(mem), kernel(kernel), ac(mem), am(mem), boss(mem), apt(mem, kernel), cam(mem), cecd(mem), cfg(mem), 
 	dsp(mem), hid(mem), frd(mem), fs(mem, kernel), gsp_gpu(mem, gpu, currentPID), gsp_lcd(mem), ldr(mem), mic(mem),
-	nim(mem), ndm(mem), ptm(mem), y2r(mem) {}
+	nim(mem), ndm(mem), ptm(mem), y2r(mem, kernel) {}
 
 static constexpr int MAX_NOTIFICATION_COUNT = 16;
 
--- src/core/services/y2r.cpp
+++ src/core/services/y2r.cpp
@@ -1,8 +1,10 @@
 #include "services/y2r.hpp"
+#include "kernel.hpp"
 
 namespace Y2RCommands {
 	enum : u32 {
 		SetTransferEndInterrupt = 0x000D0040,
+		GetTransferEndEvent = 0x000F0000,
 		PingProcess = 0x002A0000,
 		DriverInitialize = 0x002B0000
 	};
@@ -16,12 +18,14 @@ namespace Result {
 
 void Y2RService::reset() {
 	transferEndInterruptEnabled = false;
+	transferEndEvent = std::nullopt;
 }
 
 void Y2RService::handleSyncRequest(u32 messagePointer) {
 	const u32 command = mem.read32(messagePointer);
 	switch (command) {
 		case Y2RCommands::DriverInitialize: driverInitialize(messagePointer); break;
+		case Y2RCommands::GetTransferEndEvent: getTransferEndEvent(messagePointer); break;
 		case Y2RCommands::PingProcess: pingProcess(messagePointer); break;
 		case Y2RCommands::SetTransferEndInterrupt: setTransferEndInterrupt(messagePointer); break;
 		default: Helpers::panic("Y2R service requested. Command: %08X\n", command);
@@ -37,6 +41,15 @@ void Y2RService::pingProcess(u32 messagePointer) {
 void Y2RService::driverInitialize(u32 messagePointer) {
 	log("Y2R::DriverInitialize\n");
 	mem.write32(messagePointer + 4, Result::Success);
+}
+
+void Y2RService::getTransferEndEvent(u32 messagePointer) {
+	log("Y2R::GetTransferEndEvent\n");
+	if (!transferEndEvent.has_value())
+		transferEndEvent = kernel.makeEvent(ResetType::OneShot);
+
+	mem.write32(messagePointer + 4, Result::Success);
+	mem.write32(messagePointer + 12, transferEndEvent.value());
 }
 
 void Y2RService::setTransferEndInterrupt(u32 messagePointer) {
