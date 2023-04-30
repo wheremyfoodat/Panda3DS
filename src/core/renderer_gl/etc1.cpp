05cbdba9653276f03e974561b3a9c0a289e30045
wheremyfoodat <gponiris2004@gmail.com>
2023-04-30 15:16:45 +0300

    Fix typo

--- src/core/renderer_gl/etc1.cpp
+++ src/core/renderer_gl/etc1.cpp
@@ -8,7 +8,7 @@ static constexpr u32 signExtend3To32(u32 val) {
 }
 
 u32 Texture::getTexelETC(bool hasAlpha, u32 u, u32 v, u32 width, const void* data) {
-    // Pixel offset of the 8x8 tile based on u, vand the width of the texture
+    // Pixel offset of the 8x8 tile based on u, v and the width of the texture
     u32 offs = ((u & ~7) * 8) + ((v & ~7) * width);
     if (!hasAlpha)
         offs >>= 1;
