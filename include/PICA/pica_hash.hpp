b34d00952043b3a2f0d7ccb014959d43f60909f9
wheremyfoodat <gponiris2004@gmail.com>
2023-06-14 16:51:38 +0300

    Add comments explaining the hashing algorithms

--- include/PICA/pica_hash.hpp
+++ include/PICA/pica_hash.hpp
@@ -4,6 +4,10 @@
 
 // Defines to pick which hash algorithm to use for the PICA (For hashing shaders, etc)
 // Please only define one of them
+// Available algorithms:
+// xxh3: 64-bit non-cryptographic hash using SIMD, default.
+// Google CityHash64: 64-bit non-cryptographic hash, generated using regular 64-bit arithmetic
+
 //#define PANDA3DS_PICA_CITYHASH
 #define PANDA3DS_PICA_XXHASH3
 
@@ -13,4 +17,4 @@ namespace PICAHash {
 	#endif
 
 	HashType computeHash(const char* buf, std::size_t len);
-} // namespace PICAHash
+} // namespace PICAHash 
