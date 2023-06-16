2f3bc5d38e490d1d2fe25b515bd5a006ba788eb1
wheremyfoodat <gponiris2004@gmail.com>
2023-06-16 22:20:59 +0300

    [CPU] fprs() should return std::span<u32, 32> (Fixes memory corruption bug in threads.cpp)

--- include/cpu_dynarmic.hpp
+++ include/cpu_dynarmic.hpp
@@ -138,9 +138,7 @@ public:
 
 	// Get reference to array of FPRs. This array consists of the FPRs as single precision values
 	// Hence why its base type is u32
-	// Note: Dynarmic keeps 64 VFP registers as VFPv3 extends the VFP register set to 64 registers.
-	// However the 3DS ARM11 is an ARMv6k processor with VFPv2, so only the first 32 registers are actually used
-	std::span<u32, 64> fprs() { return jit->ExtRegs(); }
+	std::span<u32, 32> fprs() { return std::span(jit->ExtRegs()).first<32>(); }
 
     void setCPSR(u32 value) {
         jit->SetCpsr(value);
