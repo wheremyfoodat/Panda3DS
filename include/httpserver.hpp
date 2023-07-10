37188edc3b632df972f86e2c4c0898540d064843
offtkp <parisoplop@gmail.com>
2023-07-10 13:50:14 +0300

    Add /status command in http server

--- include/httpserver.hpp
+++ include/httpserver.hpp
@@ -1,7 +1,9 @@
 #ifdef PANDA3DS_ENABLE_HTTP_SERVER
 #pragma once
 
+#include <array>
 #include <atomic>
+#include <map>
 #include <mutex>
 
 #include "helpers.hpp"
@@ -16,7 +18,19 @@ struct HttpServer {
 	std::mutex actionMutex = {};
 	u32 pendingKey = 0;
 
+	HttpServer();
+
 	void startHttpServer();
+	std::string status();
+
+private:
+	std::map<std::string, std::pair<u32, bool>> keyMap;
+	std::array<bool, 12> pressedKeys = {};
+	bool paused = false;
+
+	u32 stringToKey(const std::string& key_name);
+	bool getKeyState(const std::string& key_name);
+	void setKeyState(const std::string& key_name, bool state);
 };
 
 #endif  // PANDA3DS_ENABLE_HTTP_SERVER
