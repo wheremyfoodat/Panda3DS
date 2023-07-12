42c86ac541f71ccac4bec618bd83a869dd6c2152
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2023-07-12 21:33:13 +0300

    Better FS::ReadDirectory

--- src/core/kernel/directory_operations.cpp
+++ src/core/kernel/directory_operations.cpp
@@ -1,3 +1,5 @@
+#include <filesystem>
+
 #include "kernel.hpp"
 
 namespace DirectoryOps {
@@ -21,7 +23,7 @@ void Kernel::closeDirectory(u32 messagePointer, Handle directory) {
 
 	const auto p = getObject(directory, KernelObjectType::Directory);
 	if (p == nullptr) [[unlikely]] {
-		Helpers::panic("Called CloseFile on non-existent file");
+		Helpers::panic("Called CloseDirectory on non-existent directory");
 	}
 
 	p->getData<DirectorySession>()->isOpen = false;
@@ -34,7 +36,64 @@ void Kernel::readDirectory(u32 messagePointer, Handle directory) {
 	const u32 outPointer = mem.read32(messagePointer + 12);
 	logFileIO("Directory::Read (handle = %X, entry count = %d, out pointer = %08X)\n", directory, entryCount, outPointer);
 	Helpers::panicDev("Unimplemented FsDir::Read");
+	
+	const auto p = getObject(directory, KernelObjectType::Directory);
+	if (p == nullptr) [[unlikely]] {
+		Helpers::panic("Called ReadDirectory on non-existent directory");
+	}
+
+	DirectorySession* session = p->getData<DirectorySession>();
+	if (!session->pathOnDisk.has_value()) [[unlikely]] {
+		Helpers::panic("Called ReadDirectory on directory that doesn't have a path on disk");
+	}
+
+	std::filesystem::path dirPath = session->pathOnDisk.value();
+
+	int count = 0;
+	while (count < entryCount && session->currentEntry < session->entries.size()) {
+		const auto& entry = session->entries[session->currentEntry];
+		std::filesystem::path path = entry.path;
+		std::filesystem::path extension = path.extension();
+		std::filesystem::path relative = path.lexically_relative(dirPath);
+		bool isDirectory = std::filesystem::is_directory(relative);
+		std::cout << "Relative path: " << relative << "\nIs directory: " << isDirectory << "\n";
+
+		std::u16string nameU16 = relative.u16string();
+		std::string nameString = relative.string();
+		std::string extensionString = extension.string();
+
+		const u32 entryPointer = outPointer + (count * 0x228); // 0x228 is the size of a single entry
+		u32 utfPointer = entryPointer;
+		u32 namePointer = entryPointer + 0x20C;
+		u32 extensionPointer = entryPointer + 0x216;
+		u32 attributePointer = entryPointer + 0x21C;
+		u32 sizePointer = entryPointer + 0x220;
+
+		for (auto c : nameU16) {
+			mem.write16(utfPointer, u16(c));
+			utfPointer += sizeof(u16);
+		}
+		mem.write16(utfPointer, 0); // Null terminate the UTF16 name
+
+		for (auto c : nameString) {
+			mem.write8(namePointer, u8(c));
+			namePointer += sizeof(u8);
+		}
+		mem.write8(namePointer, 0); // Null terminate 8.3 name
+
+		for (auto c : extensionString) {
+			mem.write8(extensionPointer, u8(c));
+			extensionPointer += sizeof(u8);
+		}
+		mem.write8(extensionPointer, 0); // Null terminate 8.3 extension
+		mem.write8(outPointer + 0x21A, 1); // Always 1 according to 3DBrew
+
+		mem.write8(attributePointer, entry.isDirectory ? 1 : 0); // "Is directory" attribute
+
+		count++;                  // Increment number of read directories
+		session->currentEntry++;  // Increment index of the entry currently being read
+	}
 
 	mem.write32(messagePointer + 4, Result::Success);
-	mem.write32(messagePointer + 8, 0);
+	mem.write32(messagePointer + 8, count);
 }
