eff62d3de7bcce3eaf076ea6582557a410678de5
Wunkolo <Wunkolo@gmail.com>
2023-07-18 22:20:20 -0700

    Disable asserts on Vulkan result codes

--- include/renderer_vk/vulkan_api.hpp
+++ include/renderer_vk/vulkan_api.hpp
@@ -5,6 +5,8 @@
 
 #define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
 #define VULKAN_HPP_NO_EXCEPTIONS
+// Disable asserts on result-codes
+#define VULKAN_HPP_ASSERT_ON_RESULT
 #include <vulkan/vulkan.hpp>
 #include <vulkan/vulkan_format_traits.hpp>
 #include <vulkan/vulkan_hash.hpp>
