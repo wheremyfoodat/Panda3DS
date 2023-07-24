6c983b16d325aecec6e0fb811f5165caed16a116
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2023-07-24 23:57:17 +0300

    [AC] Stub GetLastErrorCode

--- src/core/services/ac.cpp
+++ src/core/services/ac.cpp
@@ -3,7 +3,8 @@
 
 namespace ACCommands {
 	enum : u32 {
-		SetClientVersion = 0x00400042
+		GetLastErrorCode = 0x000A0000,
+		SetClientVersion = 0x00400042,
 	};
 }
 
@@ -12,9 +13,18 @@ void ACService::reset() {}
 void ACService::handleSyncRequest(u32 messagePointer) {
 	const u32 command = mem.read32(messagePointer);
 	switch (command) {
+		case ACCommands::GetLastErrorCode: getLastErrorCode(messagePointer); break;
 		case ACCommands::SetClientVersion: setClientVersion(messagePointer); break;
 		default: Helpers::panic("AC service requested. Command: %08X\n", command);
 	}
+}
+
+void ACService::getLastErrorCode(u32 messagePointer) {
+	log("AC::GetLastErrorCode (stubbed)\n");
+
+	mem.write32(messagePointer, IPC::responseHeader(0x0A, 2, 0));
+	mem.write32(messagePointer + 4, Result::Success);
+	mem.write32(messagePointer + 8, 0); // Hopefully this means no error?
 }
 
 void ACService::setClientVersion(u32 messagePointer) {
