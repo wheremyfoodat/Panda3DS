60655febba3df90bbbe770b0dafbe405def1d5ef
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2023-07-27 19:39:49 +0300

    [GL] Disable scissor in clearBuffer

--- src/core/renderer_gl/renderer_gl.cpp
+++ src/core/renderer_gl/renderer_gl.cpp
@@ -471,6 +471,7 @@ void RendererGL::display() {
 
 void RendererGL::clearBuffer(u32 startAddress, u32 endAddress, u32 value, u32 control) {
 	log("GPU: Clear buffer\nStart: %08X End: %08X\nValue: %08X Control: %08X\n", startAddress, endAddress, value, control);
+	gl.disableScissor();
 
 	const auto color = colourBufferCache.findFromAddress(startAddress);
 	if (color) {
