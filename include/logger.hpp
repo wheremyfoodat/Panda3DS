d3d426985a38d2d006af0171a24386aa60de81e1
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2023-08-21 16:45:35 +0300

    Add news:u

--- include/logger.hpp
+++ include/logger.hpp
@@ -50,6 +50,7 @@ namespace Log {
 	static Logger<false> ldrLogger;
 	static Logger<false> mcuLogger;
 	static Logger<false> micLogger;
+	static Logger<false> newsLogger;
 	static Logger<false> nfcLogger;
 	static Logger<false> nimLogger;
 	static Logger<false> ndmLogger;
