aaf58a00c165dff623d34810ef45edcf35f5495f
Nomi <nomiwen@outlook.de>
2023-09-26 03:34:47 +0200

    Stub new BOSS commands

--- src/core/services/service_manager.cpp
+++ src/core/services/service_manager.cpp
@@ -102,6 +102,7 @@ static std::map<std::string, Handle> serviceMap = {
 	{ "APT:A", KernelHandles::APT },
 	{ "APT:U", KernelHandles::APT },
 	{ "boss:U", KernelHandles::BOSS },
+	{ "boss:P", KernelHandles::BOSS },
 	{ "cam:u", KernelHandles::CAM },
 	{ "cecd:u", KernelHandles::CECD },
 	{ "cfg:u", KernelHandles::CFG_U },
