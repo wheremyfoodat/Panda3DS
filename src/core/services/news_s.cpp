ceb70c0a442a3c6934731cecf5a292799c5ffaeb
Nomi <nomiwen@outlook.de>
2023-09-26 03:47:20 +0200

    Add news:s service

---
+++ src/core/services/news_s.cpp
@@ -0,0 +1,15 @@
+#include "ipc.hpp"
+#include "services/news_s.hpp"
+
+namespace NewsCommands {
+	enum : u32 {};
+}
+
+void NewsSService::reset() {}
+
+void NewsSService::handleSyncRequest(u32 messagePointer) {
+	const u32 command = mem.read32(messagePointer);
+	switch (command) {
+		default: Helpers::panic("news:s service requested. Command: %08X\n", command);
+	}
+}
