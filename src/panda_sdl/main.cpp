c0c3153cd88925f9c443563bc8ee198a22a5c4f3
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2023-10-20 01:12:22 +0300

    Bonk

--- src/panda_sdl/main.cpp
+++ src/panda_sdl/main.cpp
@@ -1,11 +1,11 @@
 #include "panda_sdl/frontend_sdl.hpp"
 
 int main(int argc, char *argv[]) {
-	FrontendSDL frontend;
+	FrontendSDL app;
 
 	if (argc > 1) {
 		auto romPath = std::filesystem::current_path() / argv[1];
-		if (!frontend.loadROM(romPath)) {
+		if (!app.loadROM(romPath)) {
 			// For some reason just .c_str() doesn't show the proper path
 			Helpers::panic("Failed to load ROM file: %s", romPath.string().c_str());
 		}
@@ -13,5 +13,5 @@ int main(int argc, char *argv[]) {
 		printf("No ROM inserted! Load a ROM by dragging and dropping it into the emulator window!\n");
 	}
 
-	frontend.run();
+	app.run();
 }
