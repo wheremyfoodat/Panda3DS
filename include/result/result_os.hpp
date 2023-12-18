72d558d894611a7ddebdd304e428deb64745aa77
Nomi <nomiwen@outlook.de>
2023-12-18 18:40:28 +0100

    Add NotImplemented error code

--- include/result/result_os.hpp
+++ include/result/result_os.hpp
@@ -5,10 +5,11 @@ DEFINE_HORIZON_RESULT_MODULE(Result::OS, OS);
 
 namespace Result::OS {
 	DEFINE_HORIZON_RESULT(PortNameTooLong, 30, InvalidArgument, Usage);
-	DEFINE_HORIZON_RESULT(InvalidHandle, 1015, WrongArgument, Permanent);
 	DEFINE_HORIZON_RESULT(InvalidCombination, 1006, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(MisalignedAddress, 1009, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(MisalignedSize, 1010, InvalidArgument, Usage);
+    DEFINE_HORIZON_RESULT(NotImplemented, 1012, InvalidArgument, Usage);
+	DEFINE_HORIZON_RESULT(InvalidHandle, 1015, WrongArgument, Permanent);
 	DEFINE_HORIZON_RESULT(OutOfRange, 1021, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(Timeout, 1022, StatusChanged, Info);
 };  // namespace Result::OS
