d8a2dd69b272299e971a7f80392d89443c2a5fb4
noumidev <139501014+noumidev@users.noreply.github.com>
2023-12-18 19:29:50 +0100

    Replace spaces with tab

--- include/result/result_os.hpp
+++ include/result/result_os.hpp
@@ -8,7 +8,7 @@ namespace Result::OS {
 	DEFINE_HORIZON_RESULT(InvalidCombination, 1006, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(MisalignedAddress, 1009, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(MisalignedSize, 1010, InvalidArgument, Usage);
-    DEFINE_HORIZON_RESULT(NotImplemented, 1012, InvalidArgument, Usage);
+	DEFINE_HORIZON_RESULT(NotImplemented, 1012, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(InvalidHandle, 1015, WrongArgument, Permanent);
 	DEFINE_HORIZON_RESULT(OutOfRange, 1021, InvalidArgument, Usage);
 	DEFINE_HORIZON_RESULT(Timeout, 1022, StatusChanged, Info);
