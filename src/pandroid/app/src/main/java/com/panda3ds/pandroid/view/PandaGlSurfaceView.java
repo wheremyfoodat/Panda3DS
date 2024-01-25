c51a80a1ff8b1412ba4ee5ec348c0763af0d53a9
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-01-25 20:29:55 +0530

    PandaGlSurfaceView : pass resources if needed

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/view/PandaGlSurfaceView.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/view/PandaGlSurfaceView.java
@@ -21,7 +21,7 @@ public class PandaGlSurfaceView extends GLSurfaceView implements TouchScreenNode
 		if (Debug.isDebuggerConnected()) {
 			setDebugFlags(DEBUG_LOG_GL_CALLS);
 		}
-		renderer = new PandaGlRenderer(romPath);
+		renderer = new PandaGlRenderer(romPath, context.getResources()); // pass resources if needed
 		setRenderer(renderer);
 	}
 
