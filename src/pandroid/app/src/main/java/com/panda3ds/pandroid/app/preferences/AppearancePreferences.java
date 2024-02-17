6ee1a39fb4e5913e4f7f75004b51ff06c25c1fd0
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-02-18 02:55:48 +0530

    Add Dynamic colors option for Android 12 or above  (#8)
    * Dynamic color for android 12 or above @Ishan09811

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/preferences/AppearancePreferences.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/preferences/AppearancePreferences.java
@@ -3,6 +3,8 @@ package com.panda3ds.pandroid.app.preferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.preference.SwitchPreference;
+import com.google.android.material.color.DynamicColors;
 
 import com.panda3ds.pandroid.R;
 import com.panda3ds.pandroid.app.BaseActivity;
@@ -23,5 +25,23 @@ public class AppearancePreferences extends BasePreferenceFragment {
             GlobalConfig.set(GlobalConfig.KEY_APP_THEME, (int) value);
             return false;
         });
+        setItemClick("dynamic_colors", pref -> GlobalConfig.set(GlobalConfig.KEY_DYNAMIC_COLORS, ((SwitchPreference) pref).isChecked()));
+
+        refresh();
+        }
+    
+        @Override
+        public void onResume() {
+        super.onResume();
+        refresh();
+        }
+
+        private void refresh() {
+        ((SwitchPreference) findPreference("dynamic_colors")).setChecked(GlobalConfig.get(GlobalConfig.KEY_DYNAMIC_COLORS));
+        if (!DynamicColors.isDynamicColorAvailable()) {
+        ((SwitchPreference) findPreference("dynamic_colors")).setEnabled(false);
+        }
+            
     }
-}
+       
+ }
