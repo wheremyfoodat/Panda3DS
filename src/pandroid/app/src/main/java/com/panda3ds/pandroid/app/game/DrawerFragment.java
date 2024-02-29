aa2edaae9b97a6f2e8e9f013e2d9bb6e77cceac6
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-02-29 06:08:46 +0530

    Add Picture In Picture support with a option  (#11)
    - Ishan picture in picture pr

--- src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/game/DrawerFragment.java
+++ src/pandroid/app/src/main/java/com/panda3ds/pandroid/app/game/DrawerFragment.java
@@ -1,6 +1,7 @@
 package com.panda3ds.pandroid.app.game;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
