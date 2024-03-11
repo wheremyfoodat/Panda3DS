4b6266e3f2c59cbe56c6cdec78d732b8edd66127
Wunkolo <Wunkolo@gmail.com>
2024-03-11 12:47:59 -0700

    Initialize catch-2 based unit tests

---
+++ tests/dynapica.cpp
@@ -0,0 +1,10 @@
+#include <PICA/dynapica/shader_rec.hpp>
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "PICA/shader.hpp"
+
+#if defined(PANDA3DS_SHADER_JIT_SUPPORTED)
+TEST_CASE("Test", "[shader][dynapica]") {}
+
+#endif
