fb26e5ab0d2f9f9046fc2c7b8e88cd47a10625c9
sylvieee-iot <sylvieee.iot@gmail.com>
2024-05-02 22:43:11 +0300

    stupid

--- src/panda_sdl/main.cpp
+++ src/panda_sdl/main.cpp
@@ -30,7 +30,6 @@ int main(int argc, char *argv[]) {
 		std::vector<DeviceClass> myDevices = client.getDevices();
 		if (myDevices.size() > 0) {
 			client.sendScalar(myDevices[0], 0.5);
-			client.sendScalar(myDevices[1], 0.5);
 		}
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
