eab5fd3a44a63459cf2660e591ff30bf512fcb84
sylvieee-iot <sylvieee.iot@gmail.com>
2024-05-08 22:47:41 +0300

    I NEED SLEEP

--- src/panda_sdl/main.cpp
+++ src/panda_sdl/main.cpp
@@ -1,40 +1,7 @@
 #include "panda_sdl/frontend_sdl.hpp"
 
-#include <string>
-#include <chrono>
-#include <cstdio>
-#include <thread>
-#include "buttplugclient.h"
-
-void callbackFunction(const mhl::Messages msg) {
-	switch (msg.messageType) {
-		case mhl::MessageTypes::DeviceList: printf("Device List callback\n"); break;
-		case mhl::MessageTypes::DeviceAdded: printf("Device List callback\n"); break;
-		case mhl::MessageTypes::ServerInfo: printf("Server info callback\n"); break;
-		case mhl::MessageTypes::DeviceRemoved: printf("Device Removed callback\n"); break;
-		case mhl::MessageTypes::SensorReading: printf("Sensor reading callback\n"); break;
-		default: printf("Unknown message");
-	}
-}
-
 int main(int argc, char *argv[]) {
 	FrontendSDL app;
-	std::string url = "ws://127.0.0.1";
-	Client client(url, 12345, "test.txt");
-	client.connect(callbackFunction);
-	client.requestDeviceList();
-
-	while (1) {
-		client.startScan();
-
-		std::vector<DeviceClass> myDevices = client.getDevices();
-		if (myDevices.size() > 0) {
-			client.sendScalar(myDevices[0], 0.5);
-		}
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-		client.stopScan();
-	}
 
 	if (argc > 1) {
 		auto romPath = std::filesystem::current_path() / argv[1];
