abe0709a81618deeda53845b59600f6b347f5a0a
Samuliak <samuliak77@gmail.com>
2024-11-01 09:05:44 +0100

    don't hardcode window size

--- src/panda_sdl/frontend_sdl.cpp
+++ src/panda_sdl/frontend_sdl.cpp
@@ -88,7 +88,7 @@ FrontendSDL::FrontendSDL() : keyboardMappings(InputMappings::defaultKeyboardMapp
 
 #ifdef PANDA3DS_ENABLE_METAL
 	if (config.rendererType == RendererType::Metal) {
-		window = SDL_CreateWindow("Alber", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 480, SDL_WINDOW_METAL | SDL_WINDOW_RESIZABLE);
+		window = SDL_CreateWindow("Alber", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_METAL | SDL_WINDOW_RESIZABLE);
 
 		if (window == nullptr) {
 			Helpers::warn("Window creation failed: %s", SDL_GetError());
