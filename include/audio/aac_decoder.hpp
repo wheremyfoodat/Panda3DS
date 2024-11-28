d299081c40b775f15f811e4b5a185b8c6dfbb049
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2024-11-28 19:17:10 +0200

    AAC decoder: Audio should be enabled by default

--- include/audio/aac_decoder.hpp
+++ include/audio/aac_decoder.hpp
@@ -19,7 +19,7 @@ namespace Audio::AAC {
 	  public:
 		// Decode function. Takes in a reference to the AAC response & request, and a callback for paddr -> pointer conversions
 		// We also allow for optionally muting the AAC output (setting all of it to 0) instead of properly decoding it, for debug/research purposes
-		void decode(AAC::Message& response, const AAC::Message& request, PaddrCallback paddrCallback, bool enableAudio = false);
+		void decode(AAC::Message& response, const AAC::Message& request, PaddrCallback paddrCallback, bool enableAudio = true);
 		~Decoder();
 	};
 }  // namespace Audio::AAC
