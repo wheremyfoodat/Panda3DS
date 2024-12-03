cd0107390310d714e7228fb879f88439cb2ef189
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2024-12-03 14:16:35 +0200

    Qt: Add missing translations

--- src/panda_qt/patch_window.cpp
+++ src/panda_qt/patch_window.cpp
@@ -12,7 +12,7 @@
 #include "io_file.hpp"
 
 PatchWindow::PatchWindow(QWidget* parent) : QWidget(parent, Qt::Window) {
-	setWindowTitle("ROM patcher");
+	setWindowTitle(tr("ROM patcher"));
 
 	QVBoxLayout* layout = new QVBoxLayout;
 	layout->setContentsMargins(6, 6, 6, 6);
