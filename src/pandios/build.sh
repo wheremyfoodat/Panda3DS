cdb281c7532722b34e3f7e72467b008388b0280b
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-03-16 20:42:42 +0200

    Add iOS build script

---
+++ src/pandios/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ARCH=arm64
+CONFIGURATION=Release
+SDK=iphonesimulator18.2
+
+runSimulator=false
+
+# Go to the parent directory and build the emulator core
+cd ../..
+cmake -B build -DENABLE_VULKAN=OFF -DBUILD_HYDRA_CORE=ON -DCMAKE_TOOLCHAIN_FILE=third_party/ios_toolchain/ios.toolchain.cmake -DPLATFORM=SIMULATORARM64 -DDEPLOYMENT_TARGET="13.0"
+cmake --build build
+
+# Copy the bridging header from the emulator source to the Swift interop module folder
+cp ./include/ios_driver.h ./src/pandios/Alber/Headers/ios_driver.h
+
+# Come back to the iOS directory, build the SwiftUI xcode project and link them together
+cd src/pandios
+xcodebuild build -configuration ${CONFIGURATION} -sdk ${SDK} -arch ${ARCH}
+
+# To run the app in the simulator:
+
+# Boot the iPhone, install the app on the iphone, then open the sim & launch the app
+if [ "$runSimulator" = true ] ; then
+    xcrun simctl boot "iPhone 16"
+    xcrun simctl install "iPhone 16" "build/Release-iphonesimulator/Pandios.app"
+    open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
+    xcrun simctl launch booted "Alber.Pandios"
+fi
