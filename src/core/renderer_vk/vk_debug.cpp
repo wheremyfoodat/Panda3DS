c68cd215a1e7f44240044f73f69ea6301ccd3bbd
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-06-23 03:44:01 +0300

    Fix build on new Vk SDK
    Co-Authored-By: Nadia Holmquist Pedersen <893884+nadiaholmquist@users.noreply.github.com>

--- src/core/renderer_vk/vk_debug.cpp
+++ src/core/renderer_vk/vk_debug.cpp
@@ -61,8 +61,8 @@ namespace Vulkan {
 	}
 
 	VKAPI_ATTR VkBool32 VKAPI_CALL debugMessageCallback(
-		VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
-		const VkDebugUtilsMessengerCallbackDataEXT* callbackData, void* userData
+		vk::DebugUtilsMessageSeverityFlagBitsEXT messageSeverity, vk::DebugUtilsMessageTypeFlagsEXT messageType,
+		const vk::DebugUtilsMessengerCallbackDataEXT* callbackData, void* userData
 	) {
 		debugMessageCallback(
 			vk::DebugUtilsMessageSeverityFlagBitsEXT(messageSeverity), vk::DebugUtilsMessageTypeFlagsEXT(messageType), *callbackData
@@ -70,7 +70,7 @@ namespace Vulkan {
 		return VK_FALSE;
 	}
 
-	#ifdef GPU_DEBUG_INFO
+#ifdef GPU_DEBUG_INFO
 	void setObjectName(vk::Device device, vk::ObjectType objectType, const void* objectHandle, const char* format, ...) {
 		va_list args;
 		va_start(args, format);
