9900ed880e68b7406776fb61d707d752fd181e02
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-07-23 05:25:28 +0300

    Fix MacOS icon oopsie

--- .github/mac-bundle.sh
+++ .github/mac-bundle.sh
@@ -11,16 +11,22 @@ convert docs/img/mac_icon.ico -alpha on -background none -resize 1024x1024 PNG32
 # Apply the mask to our icon
 convert temp.png rounded_mask.png -compose DstIn -composite temp.png
 
-convert temp.png -resize 16x16    alber.iconset/icon_16x16.png
-convert temp.png -resize 32x32    alber.iconset/icon_16x16@2x.png
-convert temp.png -resize 32x32    alber.iconset/icon_32x32.png
-convert temp.png -resize 64x64    alber.iconset/icon_32x32@2x.png
-convert temp.png -resize 128x128  alber.iconset/icon_128x128.png
-convert temp.png -resize 256x256  alber.iconset/icon_128x128@2x.png
-convert temp.png -resize 256x256  alber.iconset/icon_256x256.png
-convert temp.png -resize 512x512  alber.iconset/icon_256x256@2x.png
-convert temp.png -resize 512x512  alber.iconset/icon_512x512.png
-convert temp.png -resize 1024x1024 alber.iconset/icon_512x512@2x.png
+# Normal icons
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 16x16    alber.iconset/icon_16x16.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 32x32    alber.iconset/icon_32x32.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 64x64    alber.iconset/icon_64x64.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 128x128  alber.iconset/icon_128x128.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 256x256  alber.iconset/icon_256x256.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 72 -resize 512x512  alber.iconset/icon_512x512.png
+
+# High DPI icons
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 32x32    alber.iconset/icon_16x16@2x.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 64x64    alber.iconset/icon_32x32@2x.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 128x128  alber.iconset/icon_64x64@2x.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 256x256  alber.iconset/icon_128x128@2x.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 512x512  alber.iconset/icon_256x256@2x.png
+convert temp.png -alpha on -background none -units PixelsPerInch -density 144 -resize 1024x1024 alber.iconset/icon_512x512@2x.png
+
 iconutil --convert icns alber.iconset
 rm rounded_mask.png temp.png
 
