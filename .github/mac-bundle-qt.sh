fcab5bf91e9fcebed974c83bbc893e79b28fb51d
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2025-08-12 19:52:10 +0300

    Mac bundle: Register app as game

--- .github/mac-bundle-qt.sh
+++ .github/mac-bundle-qt.sh
@@ -57,6 +57,7 @@ PlistBuddy Alber.app/Contents/Info.plist -c "add CFBundleInfoDictionaryVersion s
 PlistBuddy Alber.app/Contents/Info.plist -c "add CFBundleName string Panda3DS"
 PlistBuddy Alber.app/Contents/Info.plist -c "add CFBundlePackageType string APPL"
 PlistBuddy Alber.app/Contents/Info.plist -c "add NSHumanReadableCopyright string Copyright 2023 Panda3DS Team"
+PlistBuddy Alber.app/Contents/Info.plist -c "add LSApplicationCategoryType string public.app-category.games"
 
 PlistBuddy Alber.app/Contents/Info.plist -c "add LSMinimumSystemVersion string 10.15"
 
